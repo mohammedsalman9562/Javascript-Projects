363914fe33a6d4822e139c4319269d276b6c568c
mohammedsalman <salmanasna149@gmail.com>
2025-09-06 07:30:27 -0700

    Javascript basicis questions a to j

---
+++ 01Javascript-basics/Q&A/a-to-j.js
@@ -0,0 +1,21 @@
+// a Use alert(...); to display 'Good Morning in a popup.
+
+// b Display your name in a popup.
+
+// c Using math, calculate 10 + 5 in console.
+
+// d Calculate 20 - 5 in console.
+
+// e Calculate 2 + 2 -5 in console.
+
+// f Use document.body.innerhtml=...; to display 'Good Morning on the web page.
+
+// g Display your name on the web page.
+
+// h You order a T-shirt for $10,socks for $8 and dinner plates for $20.
+//   Use Javascript to calculate the total cost of your order.
+
+// i Your bank account hasd $100, you spend $20 on lunch, $50 on dinner,
+//   and earns $200 from your job. Calculate how much money you have.
+
+// j Use document.body.innerhtml=...; to make the web page blank.
