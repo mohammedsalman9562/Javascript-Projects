861b778458343744f45bfc0653411d2b5411b52a
mohammedsalman <salmanasna149@gmail.com>
2025-09-06 06:46:17 -0700

    The basic concept in javascript is completed i'am covering javascript bascics

---
+++ 01Javascript-basics/scripts.js
@@ -0,0 +1,15 @@
+// This code is used for html page as a popup
+
+/* 
+alert ('hello');
+alert ('Good job !');
+*/
+
+document.body.innerHTML = `<h1>I'am Starting Selflearning MERN Stack Development</h1>`;
+
+// This code ios used in nodejs terminal
+
+console.log(2+2);
+console.log(10-3);
+
+
