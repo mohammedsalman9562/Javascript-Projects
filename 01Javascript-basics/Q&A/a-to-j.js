4a45433fb3b20a07e35ca5df00877395ca2c6d97
mohammedsalman <salmanasna149@gmail.com>
2025-09-06 09:37:24 -0700

    Javascript basicis answer a to j

--- 01Javascript-basics/Q&A/a-to-j.js
+++ 01Javascript-basics/Q&A/a-to-j.js
@@ -1,21 +1,51 @@
 // a Use alert(...); to display 'Good Morning in a popup.
 
+   alert('Good Morning!');
+
 // b Display your name in a popup.
+
+   alert('Mohammed salman');
 
 // c Using math, calculate 10 + 5 in console.
 
+   console.log(10 + 5);
+   
 // d Calculate 20 - 5 in console.
+  
+     console.log(20 - 5);
 
 // e Calculate 2 + 2 -5 in console.
 
-// f Use document.body.innerhtml=...; to display 'Good Morning on the web page.
+     console.log(2 + 2 - 5);
+
+// f Use document.body.innerhtml=...; to display 'Good Morning' on the web page.
+
+    document.body.innerHTML= '<h1>Good Morning</h1>';
 
 // g Display your name on the web page.
+
+    document.body.innerHTML= '<h1>Mohammed Salman</h1>';
 
 // h You order a T-shirt for $10,socks for $8 and dinner plates for $20.
 //   Use Javascript to calculate the total cost of your order.
 
-// i Your bank account hasd $100, you spend $20 on lunch, $50 on dinner,
+     const Tshirt = 10;
+     const socks = 8;
+     const totalCost = `${Tshirt + socks}`;
+
+     console.log(`Total cost of your order $${totalCost}`);
+     
+// i Your bank account has $100, you spend $20 on lunch, $50 on dinner,
 //   and earns $200 from your job. Calculate how much money you have.
 
+     const Totalmoney = 100;
+     const Lunch = 20;
+     const Dinner = 50;
+     const Earn = 200;
+     const Total = `${Totalmoney - (Lunch + Dinner) + Earn}`;
+
+     console.log(`I have $${Total}`);
+
 // j Use document.body.innerhtml=...; to make the web page blank.
+
+   document.body.innerHTML = '';
