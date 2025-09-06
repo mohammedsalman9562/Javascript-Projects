dc6891978deaf7996dd441cfcffaa0592e9794e3
mohammedsalman <salmanasna149@gmail.com>
2025-09-06 10:20:21 -0700

    Number and Math concept is finished

---
+++ 02Numbers-&-Math/script.js
@@ -0,0 +1,27 @@
+console.log(10 * 3);
+console.log(10 / 2);
+console.log(2 + 2 + 2);
+console.log(2.2 + 2.2);
+
+const socks = 10.90;
+const ball = 20.95;
+const total = socks * 2 + ball;
+const shipping = 4.99;
+const tax = 4.77;
+
+console.log(`Total cost is $${total}`);
+console.log(`Total cost after adding shipping charge $${(total + shipping)}`);
+console.log(`Total cost after tax and shipping $${(total + shipping + tax).toFixed(2)}`);
+
+const Socks = 799;
+const Ball = 2095;
+const Total = (Socks + Ball) / 100;
+
+console.log(Total);
+console.log(`total cost $${Math.round(Total)}`);
+       
+
+
+
+
+
