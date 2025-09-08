79b20e58fe771aa5ec2f0558c40ebb3bfad1f526
mohammedsalman <salmanasna149@gmail.com>
2025-09-08 02:51:53 -0700

    String is concept is completed

---
+++ 03Strings/3Q&3A/3a-to-3n.js
@@ -0,0 +1,50 @@
+// 3a. Create the text 'My name is: ' as a string.
+
+// 3b. Create your name as a string (for example: 'Salman').
+
+// 3c. Using concatenation, add the 2 strings from 3a and 3b together to create the 
+//     text: 'My name is: ______' (replace with your name).
+
+// 3d. At a restaurant, you order 1 coffee ($5) and 1 bagel ($3). 
+//     Using math, calculate the total cost, and using concatenation, 
+//     create the text: 'Total cost: $_____' (replace with the total you calculated).
+
+// 3e. Do the same thing as 3d, but use a template string and interpolation.
+
+// 3f. Display the text from 3e in a popup using alert(...);
+
+// 3g. You order 1 coffee ($5.99) and 1 bagel ($2.95). Using math, calculate the total cost, 
+//     and using concatenation, create the text: 'Total cost: $_____' (hint: calculate in cents to avoid inaccuracies)
+
+// 3h. Do the same thing as 3g, but use a template string and interpolation.
+
+// 3i. Display the text from 3h in a popup.
+
+// 3j. Using a multi-line string, create the text from 3h and add a line of 
+// text underneath: 'Thank you, come again!'. Display both lines in a popup.
+
+/* Total cost: $8.94
+   Thank you, come again! */
+
+
+
+// Setup: in the Amazon project, update the cart to 2 basketballs ($20.95 each) with $4.99 shipping, 
+// and 2 t-shirts ($7.99 each) with $4.99 shipping. of text (use math to calculate the numbers 4 and 57.88).
+
+// Challenge Exercises
+// Items (4):               $57.88
+// Shipping & handling:      $9.98
+// Total before tax:        $67.86
+// Estimated tax (10%):      $6.79
+// Order total:             $74.65
+
+// 3k. Using interpolation, create the first line
+
+// 3l. Create second line of text: 'Shipping & handling: $9.98' (use math).
+
+// 3m. Create third line: 'Total before tax: $67.86' (use math).
+
+// 3n. Create fourth line of text: 'Estimated tax (10%): $6.79' (use math and Math.round(...); 
+// to calculate the exact number).
+
+// Finish: change the cart back to 1 basketball, 1 t-shirt (both free shipping).
---
+++ 03Strings/scripts.js
@@ -0,0 +1,26 @@
+console.log('hello');  //'hello' is a string
+
+const firstName = 'Mohammed';
+const lastName = ' Salman';
+const intial = '.s' ;
+const age = 21;
+const fullName = `My name is ${firstName + lastName}` ;
+
+console.log(fullName);
+console.log(fullName + intial);
+console.log(`${fullName + age}`);
+
+console.log(typeof(fullName));
+
+const basketball = 20.95;
+const socks = 10.90;
+const total = basketball + socks ;
+const dollar = '$';
+
+console.log(`Total price of product is : ${dollar + total}`);
+
+alert(`My name is ${firstName + lastName} Total price of product is : ${dollar + total}`);
+
+
+
+
